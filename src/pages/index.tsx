a01be9b73aa102583888cb342b9014d9baacee23
Emin Alizada <e.alizada3@gmail.com>
2024-03-26 17:34:29 +0400

    randomizer text gradient, button component added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 // import { api } from "~/utils/api";
 
-import {cn} from "~/utils/ui";
+import { cn } from "~/utils/ui";
 
 export default function Home() {
   // const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <section className={cn('min-h-screen')}>
-      hi
+    <section className={cn("min-h-screen")}>
+      <div className={cn("flex items-center justify-center p-24")}>
+        <h1
+          className={cn(
+            "text-7xl uppercase tracking-widest",
+            "inline-block bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-transparent",
+          )}
+        >
+          Randomizer
+        </h1>
+      </div>
     </section>
   );
 }
